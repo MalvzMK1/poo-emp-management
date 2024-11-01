66986e83f474b15ba81367543cd203ec1f6001d3
Matheus Alves de Oliveira <matheus.adeoliveira11@gmail.com>
2024-11-01 20:27:33 -0300

    fix: add bash flag

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
 if [ -n "$1" ] && [ "$1" = "--no-cache" ]; then
-  find $(pwd) -type d -name "*pycache*" -exec rm -r {} \; 2> /dev/null
+  find $(pwd) \
+    -type d \
+    -name "*pycache*" \
+    -exec rm -r {} \; 2> /dev/null
 fi 
 
 ./venv/bin/python main.py
